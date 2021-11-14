8ef97f5ce441f996a65b9102f57e136df9c194d6
Maira Fidalgo <mairaberlatof@gmail.com>
2021-11-13 21:37:17 -0300

    Mobile: fix server + dependências

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.103:3303'
+  baseURL: 'https://fierce-bayou-11429.herokuapp.com',
 });
 
 export default api;
